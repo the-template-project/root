79e4c5ff0d6e18d120fa7f05edc9ee6ade900874
uberandrew <uberandrew126@gmail.com>
2020-05-23 21:18:31 -0400

    Addressing comments from diff

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,29 +1,35 @@
 import React from 'react';
-import fbIcon from '../static/images/icons8-facebook-48.png';
-import twitterIcon from '../static/images/icons8-twitter-48.png';
+import fbIcon from '../static/images/f_logo_RGB-Blue_58.png';
+import twitterIcon from '../static/images/Twitter_Social_Icon_Circle_Color.png';
 import '../static/css/homepage.css';
 
 
 function Homepage() {
   return (
-    <>
+    <div>
       <header>
-        <div className="asd">
-          <h1 className="title">The Template Project</h1>
-          <p className="caption">
-            A one stop shop for beautifully designed ReactJS templates
-          </p>
+        <div className="header-text-container">
+          <h1>The Template Project</h1>
+          <p>A one stop shop for beautifully designed ReactJS templates</p>
         </div>
         <div className="social-icons-container">
-          <a href="https://www.facebook.com/">
+          <a
+            href="https://www.facebook.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img src={fbIcon} alt="fb icon" />
           </a>
-          <a href="https://www.twitter.com/">
+          <a
+            href="https://www.twitter.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img src={twitterIcon} alt="fb icon" />
           </a>
         </div>
       </header>
-    </>
+    </div>
   );
 }
 
