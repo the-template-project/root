dc39f53d2517668587a41aa018126d596a7215c0
Darius Tan <dariustan97@gmail.com>
2020-06-09 23:04:42 +1200

    Changes: 	- Use absolute positioning instead of marginRight and transform 	for carousel slide positioning. 	- Use images from unsplashed.com. 	- Remove font import. 	- Add example to front page.

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,17 +1,44 @@
-import React, { useState, useMemo, useCallback } from 'react';
+import React, { useState, useMemo, useCallback, useEffect } from 'react';
 import CarouselTrack from './CarouselTrack';
 import styles from './Carousel.module.css';
 
 export default function Carousel(props) {
     const { 
         itemsPerPage,
+        height,
+        animationDuration,
         children, 
     } = props;
 
     const _itemsPerPage = itemsPerPage || 1;
+    const _animationDuration = animationDuration || 1;
     const childrenWithKeys = useMemo(() => {
         return React.Children.toArray(children);
     }, [children]);
+    const _height = height || 400;
+    const [width, setWidth] = useState(0);
+    const measuredRef = useCallback((node) => {
+        if (node !== null) {
+            setWidth(node.getBoundingClientRect().width)
+        }
+    }, []);
+    const [windowSize, setWindowSize] = useState({
+        width: window.innerWidth,
+        height: window.innerHeight,
+        newWidth: window.innerWidth,
+        newHeight: window.innerHeight
+    });
+    useEffect(() => {
+        function handleResize() {
+            setWindowSize((windowSize) => ({
+                ...windowSize,
+                newWidth: window.innerWidth,
+                newHeight: window.innerHeight
+            }))
+        }
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, [])
     const [index, setIndex] = useState(0);
     const handleOnLeftClick = useCallback(() => {
         setIndex((index) => index === 0 ? index : index - 1);
@@ -21,32 +48,33 @@ export default function Carousel(props) {
     }, [setIndex]);
 
     return (
-        <div className={styles['carousel']}>
+        <div className={styles['carousel']}
+            ref={measuredRef}
+        >
             <CarouselTrack
                 index={index}
                 itemsPerPage={_itemsPerPage}
+                height={_height*windowSize.newHeight/windowSize.height}
+                width={width*windowSize.newWidth/windowSize.width}
+                animationDuration={_animationDuration}
             >
                 {childrenWithKeys}
             </CarouselTrack>
-            <div className={styles['carousel__buttons']}>
-                <div className={styles['carousel__buttons-container']}>
-                    <div className={styles['carousel__direction-buttons-container']}>
-                        <button style={{backgroundColor: 'blue'}} 
-                            onClick={handleOnLeftClick}
-                        />
-                        <button style={{backgroundColor: 'blue'}}
-                            onClick={handleOnRightClick}
-                        />
+            <div className={styles['carousel-buttons']}>
+                <div className={styles['carousel-buttons-container']}>
+                    <div className={styles['carousel-direction-buttons-container']}>
+                        <button onClick={handleOnLeftClick} />
+                        <button onClick={handleOnRightClick} />
                     </div>
-                    <button className={styles['carousel__eye-button']} style={{backgroundColor: '#5CD568'}}/>
-                    <button className={styles['carousel__download-button']} style={{backgroundColor: '#33C9AD'}}/>
+                    <button className={styles['carousel-eye-button']} style={{backgroundColor: '#5CD568'}}/>
+                    <button className={styles['carousel-download-button']} style={{backgroundColor: '#33C9AD'}}/>
                 </div>
             </div>
-            <div className={styles['carousel__project-text']}>
-                <p className={styles['carousel__project-text-title']}>
-                    ABOUT PROJECT
+            <div className={styles['carousel-project-text']}>
+                <p className={styles['carousel-project-text-title']}>
+                    About Project
                 </p>
-                <p className={styles['carousel__project-text-content']}>
+                <p className={styles['carousel-project-text-content']}>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
             </div>
--- src/components/Carousel/CarouselTrack.jsx
+++ src/components/Carousel/CarouselTrack.jsx
@@ -1,57 +1,55 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState, useCallback } from 'react';
 import CarouselItem from './CarouselItem';
-import styles from './CarouselTrack.module.css';
+import styles from './Carousel.module.css';
 
 export default function CarouselTrack(props) {
     const {
         index,
         itemsPerPage,
+        height,
+        width,
         animationDuration,
         children,
     } = props;
     
-    const _animationDuration = animationDuration || 1;
-
     const childrenWithKeys = useMemo(() => {
         return React.Children.toArray(children);
     }, [children]);
-
-    const maxSideLength = useMemo(() => {
-        return Math.max(index, childrenWithKeys.length-index-1);
-    }, [childrenWithKeys, index]);
-    const distancesFromCenter = useMemo(() => {
-        const distancesFromCenter = new Array(childrenWithKeys.length).fill(0);
-        distancesFromCenter[index] = maxSideLength;
-        for (let i=1; i<maxSideLength+1; ++i){
-            if (index-i >= 0) {
-                distancesFromCenter[index-i] = distancesFromCenter[index-i+1]-1;
-            }
-            if (index+i < childrenWithKeys.length) {
-                distancesFromCenter[index+i] = distancesFromCenter[index+i-1]-1;
+    const distances = useMemo(() => {
+        const distances = new Array(childrenWithKeys.length*2-1).fill(0);
+        if (childrenWithKeys.length > 0) {
+            distances[0] = childrenWithKeys.length-1;
+            for (let i=1; i<children.length*2-1; ++i){
+                distances[i] = distances[i-1]+(i<childrenWithKeys.length ? -1 : 1);
             }
         }
-        return distancesFromCenter;
-    }, [maxSideLength, childrenWithKeys, index])
-
+        return distances;
+    }, [childrenWithKeys.length])
+    const distancesFromCenter = useMemo(() => {
+        const temp = distances.slice(childrenWithKeys.length-index-1, 2*childrenWithKeys.length-index-1);
+        const minDistance = Math.min(...temp);
+        return temp.map(val => val-minDistance);
+    }, [distances, index])
     const style = {
-        'transform': `translateX(${index*(-100/itemsPerPage)}%)`,
-        'transitionProperty': 'transform',
-        'transitionDuration': `${_animationDuration}s`
-    }
-
+        'height': `${height}px`,
+    };
     return (
         <div 
-            className={styles['carousel__track']} style={style}
+            className={styles['carousel-track']}
+            style={style}
         >
             {childrenWithKeys.map((carouselItem, i) => {
+                const maxDistance = Math.max(...distancesFromCenter);
                 return (
                     <CarouselItem 
                         key={i}
-                        zIndex={distancesFromCenter[i]}
-                        brightness={(distancesFromCenter[i]+1)/(maxSideLength+1)}
-                        translateZ={(maxSideLength-distancesFromCenter[i])/(maxSideLength)}
-                        marginRight={`${(-100+100/itemsPerPage)}%`}
-                        animationDuration={_animationDuration}
+                        zIndex={maxDistance-distancesFromCenter[i]}
+                        brightness={(maxDistance-distancesFromCenter[i]+1)/(maxDistance+1)}
+                        translateZ={(maxDistance-distancesFromCenter[i])/(maxDistance)-1}
+                        leftPercentage={(i<index ? -1 : 1)*distancesFromCenter[i]/itemsPerPage}
+                        width={width}
+                        height={height}
+                        animationDuration={animationDuration}
                     >
                         {carouselItem}
                     </CarouselItem>
